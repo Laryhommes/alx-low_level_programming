b30ec2e0618145775cfb96f018cd3b0cb36ccc57
Laryhommes <laryhommes@gmail.com>
2022-09-14 03:13:22 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,23 @@
-#include "main.h"
-
-/**
- * main - prints putchar
- *return - always 0
- */
-
 int main(void)
+
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+
+	char c[0] = "_putchar";
+
+	int i;
+
+
+
+	for (i = 0; i < 9; i++)
+
+	{
+
+		_putchar(c[i]);
+
+	}
+
+	_putchar('\n');
 
 	return (0);
+
 }
