ef68a74bb63d7f6f3a502e925214a793f18b9e59
Laryhommes <laryhommes@gmail.com>
2022-09-23 04:57:12 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*_strcat -> this is function strcat
+*@dest : first param
+*@src : second param
+*Return : a string
+*/
+char *_strcat(char *dest, char *src);
+{
+int len  = 10,
